6c77c5cee7b29dadb8092e126bdbb7ea1314b467
baron.schwartz <baron.schwartz@dfb901c2-3250-0410-b216-0b33211c9131>
2011-05-30 16:07:48 +0000

    make the tcpdump grep-er able to count from the first to the last packets

--- common/SimpleTCPDumpParser.pm
+++ common/SimpleTCPDumpParser.pm
@@ -54,12 +54,27 @@ sub new {
 #  my $event = {
 #     id   => '0',                  # Sequentially assigned ID, in arrival order
 #     ts   => '1301957863.804195',  # Start timestamp
+#     ts0  => ...................   # First start timestamp
 #     end  => '1301957863.804465',  # End timestamp
+#     end1 => ...................   # Second end timestamp
 #     arg  => undef,                # For compatibility with other modules
 #     host => '10.10.18.253',       # Host IP address where the event came from
 #     port => '58297',              # TCP port where the event came from
 #     ...                           # Other attributes
 #  };
+#
+# The first and second start/end timestamps are illustrated in this timeline:
+#
+# |<--request-->|........processing time.........|<--response-->|
+# ts0           ts                               end            end1
+#
+# Normally we probably want to measure the response time from ts to end, but in
+# some cases we are interested in different ways of measuring it.
+#
+# TCP requests and responses form "sessions", which can be in one of these
+# statuses:
+#  [Q]uerying     - The remote host is sending the query to the server.
+#  [R]esponding   - The server is replying back to the remote host.
 sub parse_event {
    my ( $self, %args ) = @_;
    my @required_args = qw(next_event tell);
@@ -81,31 +96,71 @@ sub parse_event {
       my $unix_timestamp = make_ts($ts) . $us;
 
       # If it's an inbound packet, we record this as the beginning of a request.
+      # But, if there's an existing session from the $src, and that session is
+      # in "[R]esponding" status, then this must be the beginning of the *next*
+      # request from that $src.  Therefore we need to finish that request and
+      # emit an event, then forget the earlier request.
       if ( $dst =~ m/\.$self->{port}$/o ) {
-         $sessions->{$src} = {
-            pos_in_log => $pos_in_log,
-            ts         => $unix_timestamp,
-            id         => $self->{requests}++,
-         };
+         my $event;
+         if ( exists $sessions->{$src} && $sessions->{$src}->{status} eq 'R' ) {
+            # Make the event, which we'll return later.
+            $event = $self->make_event($src);
+         }
+         if ( exists $sessions->{$src} ) {
+            $sessions->{$src}->{ts} = $unix_timestamp;
+         }
+         else {
+            $sessions->{$src} ||= {
+               pos_in_log => $pos_in_log,
+               ts         => $unix_timestamp,
+               ts0        => $unix_timestamp,
+               id         => $self->{requests}++,
+               status     => 'Q',
+            };
+         }
+         return $event if $event;
       }
 
-      # If it's a reply to an inbound request, then we emit an event
-      # representing the entire request, and forget that we saw the request.
+      # If it's a reply to an inbound request, then we simply record the
+      # timestamp of the reply packet.
       elsif (defined (my $event = $sessions->{$dst}) ) {
-         my ( $src_host, $src_port ) = $dst =~ m/^(.*)\.(\d+)$/;
-         $event->{end}  = $unix_timestamp;
-         $event->{host} = $src_host;
-         $event->{port} = $src_port;
-         $event->{arg}  = undef;
-         MKDEBUG && _d('Properties of event:', Dumper($event));
-         delete $sessions->{$dst};
-         return $event;
+         $event->{status} = 'R',
+         $event->{end}  ||= $unix_timestamp;
+         $event->{end1}   = $unix_timestamp;
       }
       $pos_in_log = $tell->();
    } # EVENT
 
+   # There are probably events that have not been emitted because there's been
+   # no subsequent inbound packet from that source host/port.  Make and emit
+   # these.  We just loop over the source host/port and make events (which might
+   # not do anything but delete a session if no response has been seen yet)
+   # until we get one, which we return.
+   foreach my $src ( keys %$sessions ) {
+      my $event = $self->make_event($src);
+      return $event if $event;
+   }
+
    $args{oktorun}->(0) if $args{oktorun};
    return;
+}
+
+# Given a key into the sessions hash, makes and returns an event.  If the
+# session isn't complete (hasn't been replied to yet), returns undef.
+sub make_event {
+   my ( $self, $src ) = @_;
+   my $event = $self->{sessions}->{$src};
+   delete $self->{sessions}->{$src};
+   if ( $event->{status} eq 'R' ) {
+      my ( $src_host, $src_port ) = $src =~ m/^(.*)\.(\d+)$/;
+      $event->{host} = $src_host;
+      $event->{port} = $src_port;
+      $event->{arg}  = undef;
+      delete $event->{status};
+      MKDEBUG && _d('Properties of event:', Dumper($event));
+      return $event;
+   }
+   return undef;
 }
 
 # Function to memo-ize and cache repeated calls to timelocal.  Accepts a string,
